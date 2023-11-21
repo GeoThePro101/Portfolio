c92e9f84a9f20c5c273b38e6e179ee16b801bedb
GeoThePro101 <cogeofferson@gmail.com>
2023-11-21 11:27:58 -0800

    portfolio

---
+++ script.js
@@ -0,0 +1,15 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["BSIT Student", "Gamer", "Handsome"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+var typed = new Typed(".multi-text", {
+    strings: ["Us", "Me", "Geo"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
